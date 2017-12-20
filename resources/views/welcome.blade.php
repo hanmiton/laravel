f6f4ee46cc1449136a62d8139094a0fcad6d5afd
hanmiton <hanmilton_12@outlook.com>
2017-12-20 10:55:30 -0500

    añadiendo cambio panalla princiapl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    VETERINARIA
                 </div>
 
                 <div class="links">
