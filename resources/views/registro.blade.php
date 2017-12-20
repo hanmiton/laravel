bec48defbe82d7a8be9d9dc9dd914ebeca5568be
hanmiton <hanmilton_12@outlook.com>
2017-12-20 14:49:17 -0500

    añadiendo visa registro

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -3,16 +3,50 @@
 @section('content')
 	<div class="container col col-sm-3" >
 		<form>
-			<h2>Inicio Sesión</h2>
+			<h2>Registro</h2>
 		  <div class="form-group">
-		    <label for="formGroupExampleInput">Nombre Usuario:</label>
+		    <label for="formGroupExampleInput">Nombre:</label>
 		    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Ingrese nombre  de usuario">
 		  </div>
 		  <div class="form-group">
-		    <label for="formGroupExampleInput2">Contraseña:</label>
+		    <label for="formGroupExampleInput2">Apellido:</label>
 		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
 		  </div>
-		  <button type="submit" class="btn btn-primary">ACCEDER</button>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Dirección:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Telefono:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Celular:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Enfermedades:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Código de veterinario:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Cédula:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Usuario:</label>
+		    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="formGroupExampleInput2">Contraseña:</label>
+		    <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="Ingrese Contraseña">
+		  </div>
+		  			  
+
+		  <button type="submit" class="btn btn-primary">Continuar</button>
 	</form>
 	</div>
 	    
